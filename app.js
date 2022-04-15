7dbe27c887ce4d7ab7de05d4622522397db31f0e
axelfayet97 <axel.gayet97@gmail.com>
2022-04-15 09:00:42 +0200

    Worked on the sequelize database connection

--- app.js
+++ app.js
@@ -4,14 +4,39 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const helmet = require('helmet');
-
+const mysql = require('mysql2');
+const db = require("./models/Sequelize");
 require('dotenv').config();
+
 
 // Import routes
 const userRoute = require('./routes/user');
 const postsRoute = require('./routes/posts');
 
+// Sequelize
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+});
+// Connexion à la BDD
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.BDD_PWD,
+    database: process.env.BDD_NAME
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
 
 
+
+
+console.log("App started");
 // Export de l'application express
 module.exports = app;
