79838ff11efedb4390332a82ff5af23e49f7ed9a
Axel Fayet <axel.fayet97@gmail.com>
2022-04-20 17:29:01 +0200

    Added posts controllers

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -27,21 +27,83 @@ exports.createPost = (req, res) => {
 }
 // Retrieve all Posts from the database.
 exports.findAllPosts = (req, res) => {
-
+    Post.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving Posts."
+            });
+        });
 };
 // Find a single Post with an id
 exports.findOnePost = (req, res) => {
-
+    const id = req.params.id;
+    Post.findByPk(id)
+        .then(data => {
+            if (data) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `Cannot find Post with id=${id}.`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Post with id=" + id
+            });
+        });
 };
 // Update a Post by the id in the request
 exports.updatePost = (req, res) => {
-
+    const id = req.params.id;
+    Post.update(req.body, {
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Post was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Post with id=${id}. Maybe Post was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Post with id= " + id
+            });
+        });
 };
 // Delete a Post with the specified id in the request
 exports.deletePost = (req, res) => {
-
+    const id = req.params.id;
+    Post.destroy({
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Post was deleted successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Post with id=${id}. Maybe Post was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Post with id=" + id
+            });
+        });
 };
-// Delete all Posts from the database.
-exports.deleteAllPosts = (req, res) => {
+// Like management
+exports.postLike = (req, res) => {
 
 };
