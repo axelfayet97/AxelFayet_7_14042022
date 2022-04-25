e5e63005862aad3a11a15720c8d6d2112a3e3d0d
axelfayet97 <axel.fayet97@gmail.com>
2022-04-25 15:53:31 +0200

    Tried aother db model method

--- models/posts.model.js
+++ models/posts.model.js
@@ -1,31 +1,32 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Post extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      models.Message/*.belongsTo(models.User/*, {
-        foreingKey: {
-          allowNull: false
-        }
-      })*/
-    }
-  }
-  Post.init({
-    userId: DataTypes.STRING,
-    content: DataTypes.STRING,
-    imageUrl: DataTypes.STRING,
-    likes: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Post',
+module.exports = (sequelize, Sequelize) => {
+  const Post = sequelize.define("Post", {
+    postId: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    // userId: {
+    //   allowNull: false,
+    //   type: Sequelize.STRING,
+    //   // references: {
+    //   //   model: 'Users',
+    //   //   key: 'id'
+    //   // }
+    // },
+    content: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    // imageUrl: {
+    //   allowNull: true,
+    //   type: Sequelize.STRING,
+
+    // },
+    // likes: {
+    //   allowNull: false,
+    //   default: 0,
+    //   type: Sequelize.TINYINT
+    // }
   });
   return Post;
 };
--- models/users.model.js
+++ models/users.model.js
@@ -1,29 +1,44 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      models.User.hasMany(models.Post);
-    }
-  }
-  User.init({
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    firstname: DataTypes.STRING,
-    lastname: DataTypes.STRING,
-    bio: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'User',
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("User", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    email: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    password: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    firstname: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    lastname: {
+      allowNull: false,
+      type: Sequelize.STRING
+    },
+    bio: {
+      allowNull: true,
+      type: Sequelize.STRING
+    },
+    isAdmin: {
+      allowNull: false,
+      type: Sequelize.TINYINT,
+      default: false
+    },
+    // createdAt: {
+    //   allowNull: false,
+    //   type: Sequelize.DATE
+    // },
+    // updatedAt: {
+    //   allowNull: false,
+    //   type: Sequelize.DATE
+    // }
   });
   return User;
-};
+}
