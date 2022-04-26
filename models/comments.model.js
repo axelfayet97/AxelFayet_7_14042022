65dc387a2e94dfd517647a52088cd05fcf08c397
Axel Fayet <axel.fayet97@gmail.com>
2022-04-26 16:21:49 +0200

    Created model file for comments

---
+++ models/comments.model.js
@@ -0,0 +1,25 @@
+const Sequelize = require("sequelize");
+const db = require('../config/db.config');
+const Post = require('./posts.model');  
+const Comment = db.define("Comment", {
+    commentId: {
+        type: Sequelize.STRING,
+    },
+    userId: {
+        type: Sequelize.STRING,
+
+    },
+    content: {
+        type: Sequelize.STRING,
+
+    },
+    commentLike: {
+        type: Sequelize.STRING,
+
+    }
+});
+// Comment.belongsTo(Post, {
+//     foreingKey: "postId",
+//     as: "post"
+// })
+module.exports = Comment;
