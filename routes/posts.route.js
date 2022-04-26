b758095d454089ea3c7188ec339c3f57a496ce76
Axel Fayet <axel.fayet97@gmail.com>
2022-04-26 16:21:14 +0200

    Commented out auth middleware for dev phase

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -4,11 +4,11 @@ const postCtrl = require('../controllers/posts.controller');
 const auth = require('../middlewares/auth.middleware');
 // const multer = require('../middleware/multer-config');
 
-router.post('/', auth, postCtrl.createPost);
-router.put('/:id', auth, postCtrl.updatePost);
-router.get('/', auth, postCtrl.findAllPosts);
-router.get('/:id', auth, postCtrl.findOnePost);
-router.delete('/:id', auth, postCtrl.deletePost);
-router.post('/:id/like', auth, postCtrl.postLike)
+router.post('/', /*auth,*/ postCtrl.createPost);
+router.put('/:id', /*auth,*/ postCtrl.updatePost);
+router.get('/', /*auth,*/ postCtrl.findAllPosts);
+router.get('/:id', /*auth,*/ postCtrl.findOnePost);
+router.delete('/:id', /*auth,*/ postCtrl.deletePost);
+router.post('/:id/like', /*auth,*/ postCtrl.postLike)
 
 module.exports = router;
