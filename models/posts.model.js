34f2bde4bd2826c84ab26dec71e173af160ae834
Axel Fayet <axel.fayet97@gmail.com>
2022-04-26 10:28:33 +0200

    Fixed issue where models were not exported

--- models/posts.model.js
+++ models/posts.model.js
@@ -1,32 +1,35 @@
-module.exports = (sequelize, Sequelize) => {
-  const Post = sequelize.define("Post", {
-    postId: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      primaryKey: true,
-    },
-    // userId: {
-    //   allowNull: false,
-    //   type: Sequelize.STRING,
-    //   // references: {
-    //   //   model: 'Users',
-    //   //   key: 'id'
-    //   // }
-    // },
-    content: {
-      allowNull: false,
-      type: Sequelize.STRING
-    },
-    // imageUrl: {
-    //   allowNull: true,
-    //   type: Sequelize.STRING,
+const Sequelize = require("sequelize");
+const db = require('../config/db.config');
 
-    // },
-    // likes: {
-    //   allowNull: false,
-    //   default: 0,
-    //   type: Sequelize.TINYINT
+const Post = db.define('Post', {
+  userId: {
+    allowNull: false,
+    type: Sequelize.STRING,
+    // references: {
+    //   model: 'Users',
+    //   key: 'id'
     // }
-  });
-  return Post;
-};
+  },
+  postId: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    primaryKey: true
+
+  },
+  content: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  imageUrl: {
+    allowNull: true,
+    type: Sequelize.STRING,
+
+  },
+  likes: {
+    allowNull: true,
+    default: 0,
+    type: Sequelize.BOOLEAN
+  }
+});
+
+module.exports = Post;
--- models/users.model.js
+++ models/users.model.js
@@ -1,44 +1,38 @@
-module.exports = (sequelize, Sequelize) => {
-  const User = sequelize.define("User", {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    email: {
-      allowNull: false,
-      type: Sequelize.STRING
-    },
-    password: {
-      allowNull: false,
-      type: Sequelize.STRING
-    },
-    firstname: {
-      allowNull: false,
-      type: Sequelize.STRING
-    },
-    lastname: {
-      allowNull: false,
-      type: Sequelize.STRING
-    },
-    bio: {
-      allowNull: true,
-      type: Sequelize.STRING
-    },
-    isAdmin: {
-      allowNull: false,
-      type: Sequelize.TINYINT,
-      default: false
-    },
-    // createdAt: {
-    //   allowNull: false,
-    //   type: Sequelize.DATE
-    // },
-    // updatedAt: {
-    //   allowNull: false,
-    //   type: Sequelize.DATE
-    // }
-  });
-  return User;
-}
+const Sequelize = require("sequelize");
+const db = require('../config/db.config');
+
+const User = db.define("User", {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: Sequelize.INTEGER
+  },
+  email: {
+    allowNull: false,
+    type: Sequelize.STRING
+  },
+  password: {
+    allowNull: false,
+    type: Sequelize.STRING
+  },
+  firstname: {
+    allowNull: false,
+    type: Sequelize.STRING
+  },
+  lastname: {
+    allowNull: false,
+    type: Sequelize.STRING
+  },
+  bio: {
+    allowNull: true,
+    type: Sequelize.STRING
+  },
+  isAdmin: {
+    allowNull: false,
+    type: Sequelize.TINYINT,
+    default: false
+  },
+});
+
+module.exports = User;
