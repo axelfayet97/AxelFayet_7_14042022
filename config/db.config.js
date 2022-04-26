8b7a9685d8367fca5412488b4843c927897f78b9
Axel Fayet <axel.fayet97@gmail.com>
2022-04-26 10:15:25 +0200

    Renamed file db, simplified file

---
+++ config/db.config.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+require('dotenv').config();
+
+// Connexion à la base de données
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  operatorsAliases: 0,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+module.exports = db;
