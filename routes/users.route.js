4898397e0eec9a22bb209e3d94522d7215885345
Axel Fayet <axel.fayet97@gmail.com>
2022-04-27 15:44:10 +0200

    Deleted files in order to re-organize them

--- routes/users.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userCtrl = require('../controllers/users.controller');
-
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
-
-module.exports = router;
