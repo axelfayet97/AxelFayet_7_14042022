ab82a225b3a6c0a04c49e2cfd055dea822462e4a
axelfayet97 <axel.gayet97@gmail.com>
2022-04-27 19:24:35 +0200

    Divided server file in order to have a dedicated server file and a dedicated app file

--- server.js
+++ server.js
@@ -1,35 +1,5 @@
 const http = require('http');
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const nocache = require('nocache');
-const bodyParser = require('body-parser');
-const app = express();
-const db = require('./app/config/db');
-const controller = require('./app/controllers/post.controller');
-// À utiliser pour envoyer des données en dur avec la fonction run() lors de la synchro à la bdd
-// const run = async () => {
-// };
-var corsOptions = {
-    origin: '*'
-};
-app.use(cors(corsOptions));
-// Parse les requêtes de application/json
-app.use(bodyParser.json());
-app.use(express.json());
-// Parse les requêtes de application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-// Sécurité helmet et désactivation du cache
-app.use(helmet({ crossOriginResourcePolicy: false }));
-app.use(nocache());
-// Route test
-app.get('/', (req, res) => {
-    res.json({ message: 'API connected' });
-});
-db.sequelize.sync({ force: false }).then(() => {
-    console.log('Drop and re-sync db.');
-    // run();
-});
+const app = require('./app');
 // Import des routes
 require('./app/routes/post.routes')(app);
 require('./app/routes/user.routes')(app);
