e5afba5b3f2e1c7ce9390742984133f486ebe238
Axel Fayet <axel.fayet97@gmail.com>
2022-04-27 16:15:47 +0200

    Modified post model

--- app/models/post.model.js
+++ app/models/post.model.js
@@ -1,11 +1,12 @@
 module.exports = (sequelize, Sequelize) => {
     const Post = sequelize.define('post', {
-        title: {
+        content: {
             type: Sequelize.STRING
         },
-        description: {
-            type: Sequelize.STRING
-        },
+        imageUrl: {
+            type: Sequelize.STRING,
+            allowNull: true
+        }
     });
     return Post;
 };
