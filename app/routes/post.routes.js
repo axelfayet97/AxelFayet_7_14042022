bbe05fad6a59f02662a93edd10ae2b5a057ee660
axelfayet97 <axel.gayet97@gmail.com>
2022-04-27 21:01:59 +0200

    Renamed post routes, added auth middleware

--- app/routes/post.routes.js
+++ app/routes/post.routes.js
@@ -1,15 +1,17 @@
 module.exports = app => {
     const posts = require('../controllers/post.controller.js');
+    const auth = require('../middlewares/auth.middleware');
     var router = require('express').Router();
+    
     // Create a new Post
-    router.post('/', posts.createPost);
+    router.post('/', /*auth,*/ posts.createPost);
     // Retrieve all Posts
-    router.get('/', posts.findAll);
+    router.get('/', posts.findAllPosts);
     // Retrieve a single Post with id
-    router.get('/:id', posts.findOne);
+    router.get('/:id', posts.findOnePost);
     // Update a Post with id
-    router.put('/:id', posts.update);
+    router.put('/:id', posts.updatePost);
     // Delete a Post with id
-    router.delete('/:id', posts.delete);
+    router.delete('/:id', posts.deletePost);
     app.use('/api/posts', router);
 };
