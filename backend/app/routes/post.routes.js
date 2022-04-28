dd1fb844dcc0faf250203137ffb6e930209579ee
axelfayet97 <axel.gayet97@gmail.com>
2022-04-28 13:07:57 +0200

    Routed post controller

--- backend/app/routes/post.routes.js
+++ backend/app/routes/post.routes.js
@@ -1,17 +1,17 @@
 module.exports = app => {
-    const posts = require('../controllers/post.controller.js');
+    const posts = require('../controllers/post.controller');
     const auth = require('../middlewares/auth.middleware');
     var router = require('express').Router();
-    
+
     // Create a new Post
-    router.post('/', /*auth,*/ posts.createPost);
+    router.post('/', auth, posts.createPost);
     // Retrieve all Posts
     router.get('/', posts.findAllPosts);
     // Retrieve a single Post with id
     router.get('/:id', posts.findOnePost);
     // Update a Post with id
-    router.put('/:id', posts.updatePost);
+    router.put('/:id', auth, posts.updatePost);
     // Delete a Post with id
-    router.delete('/:id', posts.deletePost);
+    router.delete('/:id', auth, posts.deletePost);
     app.use('/api/posts', router);
 };
