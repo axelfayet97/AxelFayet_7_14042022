e83013ebff673faa1acab9c2b08d5f093a59ad65
axelfayet97 <axel.gayet97@gmail.com>
2022-04-28 13:06:36 +0200

    Imported dotenv package elsewhere, set dialect to be read in env variables

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -1,11 +1,10 @@
-require('dotenv').config();
 // Connexion à la base de données
 module.exports = {
     HOST: process.env.DB_HOST,
     USER: process.env.DB_USER,
     PASSWORD: process.env.DB_PWD,
     DB: process.env.DB_NAME,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
     pool: {
         max: 5,
         min: 0,
