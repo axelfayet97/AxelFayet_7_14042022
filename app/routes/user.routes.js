da7d0d1a5fb9a5e4b39cee85f9e4ad00da92aff5
axelfayet97 <axel.gayet97@gmail.com>
2022-04-28 10:35:05 +0200

    Moved filed in backend folder

--- app/routes/user.routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    const userCtrl = require('../controllers/user.controller');
-    var router = require('express').Router();
-    router.post('/signup', userCtrl.signup);
-    router.post('/login', userCtrl.login);
-    app.use('/api/auth', router);
-}
