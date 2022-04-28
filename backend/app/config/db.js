284812f76f46ee0c9a8e043001bac0a1d26f3aba
axelfayet97 <axel.gayet97@gmail.com>
2022-04-28 13:07:09 +0200

    Improved comments, defined relations between comments, likes, posts & users

--- backend/app/config/db.js
+++ backend/app/config/db.js
@@ -1,5 +1,6 @@
 const dbConfig = require('./db.config.js');
 const Sequelize = require('sequelize');
+// DATABASE CONNECTION
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
     host: dbConfig.HOST,
     dialect: dbConfig.dialect,
@@ -10,21 +11,48 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
         acquire: dbConfig.pool.acquire,
         idle: dbConfig.pool.idle,
     },
+    logging: false
 });
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
-db.users = require('../models/user.model.js')(sequelize, Sequelize);
-db.posts = require('../models/post.model.js')(sequelize, Sequelize);
-db.comments = require('../models/comment.model.js')(sequelize, Sequelize);
+// MODELS
+db.users = require('../models/user.model')(sequelize, Sequelize);
+db.posts = require('../models/post.model')(sequelize, Sequelize);
+db.comments = require('../models/comment.model')(sequelize, Sequelize);
+db.likes = require('../models/like.model')(sequelize, Sequelize);
+// RELATIONS
 db.users.hasMany(db.posts, { as: 'posts' });
+db.users.hasMany(db.comments, { as: 'comments' });
+db.users.hasMany(db.likes, { as: 'likes' });
+// Posts relations
 db.posts.belongsTo(db.users, {
     foreignKey: 'userId',
     as: 'user'
 });
 db.posts.hasMany(db.comments, { as: 'comments' });
+db.posts.hasMany(db.likes, { as: 'likes' });
+// Comments relations
+db.comments.belongsTo(db.users, {
+    foreignKey: 'userId',
+    as: 'user',
+    onDelete: 'CASCADE'
+})
 db.comments.belongsTo(db.posts, {
     foreignKey: 'postId',
     as: 'post',
+    onDelete: 'CASCADE'
 });
+// Likes relations
+db.likes.belongsTo(db.users, {
+    foreignKey: 'userId',
+    as: 'user',
+    onDelete: 'CASCADE'
+});
+db.likes.belongsTo(db.posts, {
+    foreignKey: 'postId',
+    as: 'post',
+    onDelete: 'CASCADE'
+});
+// DB EXPORT
 module.exports = db;
