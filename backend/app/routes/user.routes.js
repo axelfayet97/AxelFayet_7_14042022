9234f921df7912909bea2a76b1ac3349bc823590
axelfayet97 <axel.fayet97@gmail.com>
2022-05-01 14:32:32 +0200

    Added auth & multer middleware to put route, created get routes for dev

--- backend/app/routes/user.routes.js
+++ backend/app/routes/user.routes.js
@@ -1,8 +1,14 @@
 // ROUTE POUR LES UTILISATEURS
 module.exports = app => {
     const userCtrl = require('../controllers/user.controller');
+    const auth = require('../middlewares/auth.middleware');
+    const multer = require('../middlewares/multer.middleware');
     var router = require('express').Router();
     router.post('/signup', userCtrl.signup);
     router.post('/login', userCtrl.login);
+    router.put('/:id', auth, multer, userCtrl.modifyAccount);
+    router.get('/', userCtrl.getAccounts);
+    router.get('/:id', userCtrl.getOneAccount);
+    // router.delete('/:id', auth, userCtrl.deleteAccount); // DISABLE NOT DELETE
     app.use('/api/auth', router);
 }
