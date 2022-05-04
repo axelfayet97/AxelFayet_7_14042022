9da790e1361f7e04daea07150dc67f12755f279c
axelfayet97 <axel.fayet97@gmail.com>
2022-05-04 15:41:48 +0200

    Updated like model to be an integer rather than a boolean

--- backend/app/models/like.model.js
+++ backend/app/models/like.model.js
@@ -3,8 +3,8 @@ module.exports = (sequelize, Sequelize) => {
     const Like = sequelize.define('like', {
         isLiked: {
             allowNull: true,
-            type: Sequelize.BOOLEAN,
-            default: false
+            type: Sequelize.INTEGER,
+            default: 0
         }
     });
     return Like;
