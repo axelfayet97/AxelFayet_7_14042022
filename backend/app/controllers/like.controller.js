569bfb65814e0feb424a72e4bff488bf851f43fb
axelfayet97 <axel.fayet97@gmail.com>
2022-05-04 15:40:47 +0200

    Started working on creating and getting likes

--- backend/app/controllers/like.controller.js
+++ backend/app/controllers/like.controller.js
@@ -1,7 +1,56 @@
-const { posts } = require('../config/db');
 const db = require('../config/db');
 const Like = db.likes;
 
-exports.likesControl = (req, res) => {
-
+exports.likeState = (req, res) => {
+    Like.findAll({
+        where: {
+            postId: req.body.postId,
+            userId: req.body.userId
+        }
+    })
+        .then(likes => {
+            if (likes.length === 0) {
+                const like = new Like({
+                    ...req.body
+                    // User ID ?
+                });
+                // Enregistrement de l'objet like dans la base de données
+                like.save()
+                    .then(() => {
+                        Like.findAll({
+                            where: { postId: req.body.postId }
+                        }).then(likes => {
+                            res.status(200).json({ like: likes.length });
+                        })
+                    })
+                    .catch(error => res.status(400).json({ error }));
+            } else {
+                Like.destroy({
+                    where: {
+                        postId: req.body.postId,
+                        userId: req.body.userId
+                    }
+                })
+                    .then(() => {
+                        Like.findAll({
+                            where: { postId: req.body.postId }
+                        }).then(likes => {
+                            res.status(200).json({ like: likes.length });
+                        })
+                    })
+                    .catch(error => res.status(400).json({ error }));
+            }
+        })
+};
+exports.getLikes = (req, res) => {
+    Like.findAll({
+        where: {
+            postId: req.params.id
+        }
+    })
+        .then(likes => {
+            console.log(likes);
+            res.status(200).send({ data: likes });
+        })
+        .catch(error => res.status(400).send(error));
 };
