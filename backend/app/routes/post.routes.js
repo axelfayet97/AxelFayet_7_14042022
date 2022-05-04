7d06c9adc5b3af78ed65ca73544d0af11762d76b
axelfayet97 <axel.fayet97@gmail.com>
2022-05-04 12:46:49 +0200

    Deleted useless comments

--- backend/app/routes/post.routes.js
+++ backend/app/routes/post.routes.js
@@ -3,16 +3,10 @@ module.exports = app => {
     const posts = require('../controllers/post.controller');
     const auth = require('../middlewares/auth.middleware');
     var router = require('express').Router();
-
-    // Create a new Post
     router.post('/', auth, posts.createPost);
-    // Retrieve all Posts
     router.get('/', posts.findAllPosts);
-    // Retrieve a single Post with id
     router.get('/:id', posts.findOnePost);
-    // Update a Post with id
     router.put('/:id', auth, posts.updatePost);
-    // Delete a Post with id
     router.delete('/:id', auth, posts.deletePost);
     app.use('/api/posts', router);
 };
