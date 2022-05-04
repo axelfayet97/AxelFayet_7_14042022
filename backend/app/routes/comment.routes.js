b48620fed390333dba27c26579384216bfbb057d
axelfayet97 <axel.fayet97@gmail.com>
2022-05-04 15:42:29 +0200

    Tried to find a way to make comments working

--- backend/app/routes/comment.routes.js
+++ backend/app/routes/comment.routes.js
@@ -1,16 +1,11 @@
 // ROUTE POUR LES COMMENTAIRES
 module.exports = app => {
-    const comments = require('../controllers/comment.controller');
+    const comCtrl = require('../controllers/comment.controller');
     // const auth = require('../middlewares/auth.middleware');
     var router = require('express').Router();
-
-    // Create a new Comment
-    router.post('/', auth, comments.createComment);
-    // Retrieve a single Post with id
-    router.get('/', comments.findCommentById);
-    // Update a Post with id
-    router.put('/:id', auth, comments.updateComment);
-    // Delete a Post with id
-    router.delete('/:id', auth, comments.deleteComment)
-    app.use('/api/posts/', router);
+    router.post('/', comCtrl.createComment);
+    router.get('/', comCtrl.getAllComments);
+    router.get('/', comCtrl.findCommentById);
+    router.delete('/:id', /*auth,*/ comCtrl.deleteComment);
+    app.use('/api/comments', router);
 };
