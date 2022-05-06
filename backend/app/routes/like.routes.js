2450e77f036e7c60345105f6059e2b5c3bcc770b
axelfayet97 <axel.fayet97@gmail.com>
2022-05-06 10:49:01 +0200

    Renamed file

--- backend/app/routes/like.routes.js
+++ backend/app/routes/like.routes.js
@@ -1,9 +1,9 @@
 // ROUTE POUR LES LIKES
 module.exports = app => {
     const likesCtrl = require('../controllers/like.controller');
-    // const auth = require('../middlewares/auth.middleware');
+    const auth = require('../middlewares/auth.middleware');
     var router = require('express').Router();
-    router.post('/', /*auth,*/likesCtrl.likeState);
+    router.post('/', auth, likesCtrl.likeState);
     router.get('/', likesCtrl.getLikes)
     app.use('/api/likes', router);
 };
