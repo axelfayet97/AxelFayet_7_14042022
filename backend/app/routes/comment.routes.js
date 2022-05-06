fb6374308cffa643f2738f003b772da673401545
axelfayet97 <axel.fayet97@gmail.com>
2022-05-06 12:12:24 +0200

    Updated comment routes + auth

--- backend/app/routes/comment.routes.js
+++ backend/app/routes/comment.routes.js
@@ -1,11 +1,11 @@
 // ROUTE POUR LES COMMENTAIRES
 module.exports = app => {
     const comCtrl = require('../controllers/comment.controller');
-    // const auth = require('../middlewares/auth.middleware');
+    const auth = require('../middlewares/auth.middleware');
     var router = require('express').Router();
-    router.post('/', comCtrl.createComment);
+    router.post('/', auth, comCtrl.createComment);
     router.get('/', comCtrl.getAllComments);
-    router.get('/', comCtrl.findCommentById);
-    router.delete('/:id', /*auth,*/ comCtrl.deleteComment);
+    router.get('/:id', comCtrl.findCommentById);
+    router.delete('/:id', auth, comCtrl.deleteComment);
     app.use('/api/comments', router);
 };
