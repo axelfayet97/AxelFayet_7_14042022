a092cd1c7735a5394f86f02ac674dd7cce14e200
axelfayet97 <axel.fayet97@gmail.com>
2022-05-06 12:12:41 +0200

    Updated like get one thing route

--- backend/app/routes/like.routes.js
+++ backend/app/routes/like.routes.js
@@ -4,6 +4,6 @@ module.exports = app => {
     const auth = require('../middlewares/auth.middleware');
     var router = require('express').Router();
     router.post('/', auth, likesCtrl.likeState);
-    router.get('/', likesCtrl.getLikes)
+    router.get('/:id', likesCtrl.getLikes)
     app.use('/api/likes', router);
 };
