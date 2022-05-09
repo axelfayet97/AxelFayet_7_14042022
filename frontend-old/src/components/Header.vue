1774b6a0616c1e7e95613af99a9af8b73d659503
Axel Fayet <axel.fayet97@gmail.com>
2022-05-09 17:38:46 +0200

    Trying another way to manage Vue application

---
+++ frontend-old/src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+<header class="header__wrapper">
+    <div class="header__wrapper__col">
+        <a href="#">
+            <img src="./icons/icon-left-font.jpeg" />
+        </a>
+    </div>
+</header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+
+</script>
+
+<style >
+</style>
