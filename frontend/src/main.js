df4d14b65a7a0b5f42aac4cedd7cae94a4b2241d
Axel Fayet <axel.fayet97@gmail.com>
2022-05-12 17:19:28 +0200

    Removed useless comments @ imported Axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,74 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
-
+import './axios'
 import './assets/main.css'
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-
-// let app = Vue.createApp({
-    // data: function () {
-        // return {
-            // message: 'Hello Vue!',
-        // }
-    // },
-    // methods: {
-    // }
-// })
-// app.component('header', {
-    // template: `
-    // 
-    // <div class="header__wrapper__col">
-        // <a href="#">
-            // <img src="./icons/icon-left-font.jpeg" />
-        // </a>
-    // </div>
-// `,
-    // data() {
-        // return {
-            // title: 'Header'
-        // }
-    // }
-// });
-// app.component('login-form', {
-    // template: `
-    //    <form class="form_wrapper" method="post" action="../views/Home.vue">
-    // <div class="form_wrapper__field email__field">
-    //   <label for="email">Votre adresse mail</label>
-    //   <input type="text" id="email" placeholder="Votre adresse mail" />
-    //   <p v-if="fieldError == true">{{ errorMessage }}</p>
-    // </div>
-    // <div class="form_wrapper__field password__field">
-    //   <label for="password">Votre mot de passe</label>
-    //   <input type="password" id="password" placeholder="Votre mot de passe" />
-    //   <p v-if="fieldError == true">{{ errorMessage }}</p>
-    // </div>
-    // <div class="form_wrapper__button submit__button">
-    //   <input type="submit" id="submit" value="C'est parti !"  />
-    // </div>
-//   </form>
-    //   `,
-    // data() {
-        // return {
-            // title: 'Login Form'
-        // }
-    // }
-// })
-// app.component('footer', {
-    // template: `
-        // <ul>
-            // <li>© 2022</li>
-            // <li>Groupomania</li>
-            // <li>Tous droits réservés</li>
-            // <li><a href="#">Politique de confidentialité</a></li>
-            // <li><a href="#">Mentions légales</a></li>
-        // </ul> 
-// `,
-    // data() {
-        // return {
-            // title: 'Footer'
-        // }
-    // }
-// });
