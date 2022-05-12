2feea13ec45225ad7f9cd214a2c3871257906a4d
Axel Fayet <axel.fayet97@gmail.com>
2022-05-12 17:17:54 +0200

    Created post template file

---
+++ frontend/src/components/Post.vue
@@ -0,0 +1,25 @@
+<template>
+    <article class="post-container">
+        <div class="post-container__header">
+            <div class="post-container__header__user-infos">
+                <img src="" alt="" id="author-img">
+                <p id="post-author"></p>
+            </div>
+            <div class="post-container__header__options"></div>
+        </div>
+        <div class="post-container__body">
+            <p>Bonjour</p>
+        </div>
+        <div class="post-container__controls">
+            <span id="like-post"></span>
+        </div>
+    </article>
+</template>
+
+<script>
+
+export default {
+    props: ['post'],
+   
+}
+</script>
