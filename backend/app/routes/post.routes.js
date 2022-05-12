1b70122077851c3cae14447456cfe7fd414d0f16
Axel Fayet <axel.fayet97@gmail.com>
2022-05-12 17:26:24 +0200

    Added auth on get post routes

--- backend/app/routes/post.routes.js
+++ backend/app/routes/post.routes.js
@@ -4,8 +4,8 @@ module.exports = app => {
     const auth = require('../middlewares/auth.middleware');
     var router = require('express').Router();
     router.post('/', auth, posts.createPost);
-    router.get('/', posts.findAllPosts);
-    router.get('/:id', posts.findOnePost);
+    router.get('/', auth, posts.findAllPosts);
+    router.get('/:id', auth, posts.findOnePost);
     router.put('/:id', auth, posts.updatePost);
     router.delete('/:id', auth, posts.deletePost);
     app.use('/api/posts', router);
