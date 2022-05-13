b14e7c8a93f52471ba6708f65e751f5776e2a5ee
Axel Fayet <axel.fayet97@gmail.com>
2022-05-13 17:37:52 +0200

    Worked on home view

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,24 +1,27 @@
 <template>
   <h1>Bienvenue sur la home</h1>
-  <h2 v-if="user">Bonjour {{ user.firstName }} {{ user.lastName }}</h2>
+  <h2>Bonjour </h2>
   <section id="create-post">
     <div class="form_wrapper">
       <div class="form_wrapper_background">
-        <form class="connection_form">
+        <form class="connection_form"
+              @submit.prevent="sendPost">
           <div id="textarea"
                class="form_wrapper__field text_field">
             <label for="text">Rédigez un post</label>
             <input type="text"
                    name="text"
                    id="text"
-                   placeholder="À quoi pensez vous ?">
+                   placeholder="À quoi pensez vous ?"
+                   :v-model="content"
+                   required />
           </div>
           <div id="textarea">
             <label for="file">Ajoutez une image</label>
             <input type="file"
                    name="file"
                    id="file"
-                   placeholder="" />
+                   :v-model="imageUrl" />
           </div>
           <div class="form_wrapper__button submit__button">
             <input type="submit"
@@ -29,36 +32,45 @@
       </div>
     </div>
   </section>
-  <section @load="posts"
-           class="posts"
+  <!-- <section class="posts"
            id="all-posts">
     <p v-if="!isPosts"
        id="no-post">There's no post for the moment !</p>
-    <Post v-if="isPosts"
+    <Post v-else
           class="single-post" />
-  </section>
+  </section> -->
 </template>
 
 <script>
-import Post from "../components/Post.vue"
-import axios from "axios"
+import Post from '../components/Post.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  async created() {
-    const response = await axios.get('posts')
-    this.user = response.data
+  data() {
+    return {
+      email: '',
+      password: '',
+      content: '',
+      imageUrl: ''
+    }
   },
   components: {
     Post
   },
   methods: {
-
-  },
-  data() {
-    return {
-      user: null
+    async sendPost() {
+      await axios.post('posts',
+        {
+          content: this.content,
+          imageUrl: this.imageUrl
+        })
+      this.$router.push('/')
     }
-  }
+  },
+  // mounted:
+  //   async function getAllPosts() {
+  //     await axios.get('posts')
+  //   }
 }
 </script>
