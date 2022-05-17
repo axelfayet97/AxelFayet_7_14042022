d9e6ab55a4d7b24ea6886dc57b5d2a256a8b512b
axelfayet97 <axel.gayet97@gmail.com>
2022-05-17 18:11:17 +0200

    Working on posts content display

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,17 +1,22 @@
 <template>
-    <article class="post-container">
+    <article class="post-container" v-for="(post, i) in posts"
+          :key="i">
         <div class="post-container__header">
             <div class="post-container__header__user-infos">
-                <img src="" alt="" id="author-img">
-                <p id="post-author"></p>
+                <img src=""
+                     alt="Photo de profil"
+                     id="author-img">
+                <p id="post-author"> {{ posts[i].content }} {{ posts.lastName }}</p>
+                <p id="created-at">{{ posts.createdAt }}</p>
             </div>
-            <div class="post-container__header__options"></div>
+            <div class="post-container__header__options">...</div>
         </div>
         <div class="post-container__body">
-            <p>Bonjour</p>
+            <p>{{ posts.content }}</p>
+            <p v-if="posts.imageUrl != null">{{ posts.imageUrl }}</p>
         </div>
         <div class="post-container__controls">
-            <span id="like-post"></span>
+            <span id="like-post">{{ posts.likes }}</span>
         </div>
     </article>
 </template>
@@ -19,7 +24,11 @@
 <script>
 
 export default {
-    props: ['post'],
-   
+    name: 'PostTemplate',
+    data() {
+        return {
+            posts: []
+        }
+    }
 }
 </script>
