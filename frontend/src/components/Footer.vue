f6d89041e30e5f2625e11638f4610de62848e2b5
axelfayet97 <axel.gayet97@gmail.com>
2022-05-18 21:13:25 +0200

    Updated footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -4,13 +4,12 @@
       <li>© 2022</li>
       <li>Groupomania</li>
       <li>Tous droits réservés</li>
-      <li><a href="#">Politique de confidentialité</a></li>
-      <li><a href="#">Mentions légales</a></li>
+      <li><a href="mailto:contact@groupomania.fr">Contact</a></li>
     </ul>
   </footer>
 </template>
 <script>
 export default {
   name: "Footer",
-};
+}
 </script>
