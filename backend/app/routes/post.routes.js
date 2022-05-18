a2917854c0d129866c9c011a700f32e407ab55d5
axelfayet97 <axel.gayet97@gmail.com>
2022-05-18 15:21:34 +0200

    Added multer

--- backend/app/routes/post.routes.js
+++ backend/app/routes/post.routes.js
@@ -2,8 +2,9 @@
 module.exports = app => {
     const posts = require('../controllers/post.controller');
     const auth = require('../middlewares/auth.middleware');
+    const multer = require('../middlewares/multer.middleware');
     var router = require('express').Router();
-    router.post('/', auth, posts.createPost);
+    router.post('/', auth, multer, posts.createPost);
     router.get('/', auth, posts.findAllPosts);
     router.get('/:id', auth, posts.findOnePost);
     router.put('/:id', auth, posts.updatePost);
