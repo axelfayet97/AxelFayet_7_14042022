093308ee721f5e704341f44af19e32e69c13380d
axelfayet97 <axel.gayet97@gmail.com>
2022-05-18 15:23:33 +0200

    Worked on post vue - ok

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,33 +1,53 @@
 <template>
-    <article class="post-container" v-for="(post, i) in posts"
-          :key="i">
+    <article class="post-container"
+             v-for="post in posts"
+             :key="post.id">
         <div class="post-container__header">
             <div class="post-container__header__user-infos">
                 <img src=""
                      alt="Photo de profil"
                      id="author-img">
-                <p id="post-author"> {{ posts[i].content }} {{ posts.lastName }}</p>
-                <p id="created-at">{{ posts.createdAt }}</p>
+                <p id="post-author"> {{ post.firstName }} {{ post.lastName }}</p>
+                <p id="created-at">{{ post.createdAt }}</p>
             </div>
             <div class="post-container__header__options">...</div>
         </div>
         <div class="post-container__body">
-            <p>{{ posts.content }}</p>
-            <p v-if="posts.imageUrl != null">{{ posts.imageUrl }}</p>
+            <p>{{ post.content }}</p>
+            <p v-if="post.imageUrl != null">{{ post.imageUrl }}</p>
         </div>
         <div class="post-container__controls">
-            <span id="like-post">{{ posts.likes }}</span>
+            <span id="like-post"><a href="#"
+                   id="like-button"
+                   @click="likePost">Add Like</a> {{ post.likes.length }}</span>
         </div>
     </article>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
     name: 'PostTemplate',
     data() {
         return {
-            posts: []
+            posts: [],
+            isLiked: 0
+        }
+    },
+    async beforeMount() {
+        const response = await axios.get('posts')
+        const posts = await response.data
+        console.log(posts);
+        this.posts = posts
+    },
+    methods: {
+        async likePost() {
+            // Récupérer l'id du post à liker
+            const response = await axios.post('likes')
+            const likes = await response.data
+            console.log(likes)
+            this.isLiked = likes
         }
     }
 }
