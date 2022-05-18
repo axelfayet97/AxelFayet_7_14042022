8f66bdf83745f7c2780b103d7a1c6445e4a565ab
axelfayet97 <axel.gayet97@gmail.com>
2022-05-18 15:20:28 +0200

    Imported path and use multer

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const nocache = require('nocache');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const db = require('./app/config/db');
 // Définition des propriétés CORS
@@ -19,6 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 // Sécurité helmet et désactivation du cache
 app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(nocache());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 // SYNCHRONISATION A LA BDD : DEFINIR FORCE SUR TRUE POUR PURGER
 db.sequelize.sync({ force: false }).then(() => {
     console.log('Drop and re-sync db.');
