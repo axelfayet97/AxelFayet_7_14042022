759554bbf624837df95a6bf9be97a74babb1fa11
axelfayet97 <axel.gayet97@gmail.com>
2022-05-18 21:17:45 +0200

    Protected un authentified routes

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -2,19 +2,44 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Login from '@/views/Login.vue'
 import Signup from '@/views/Signup.vue'
+import UserProfile from '@/views/UserProfile.vue'
 
 const routes = [
     {
+        requireAuth: true,
+        beforeEnter: (to, from, next) => {
+            if (localStorage.getItem('token') == null) {
+                next({ name: 'login' })
+            } else {
+                next()
+            }
+        },
+        name: 'home',
         path: '/',
         component: Home
     },
     {
+        name: 'login',
         path: '/login',
         component: Login,
     },
     {
+        name: 'signup',
         path: '/signup',
         component: Signup
+    },
+    {
+        requireAuth: true,
+        beforeEnter: (to, from, next) => {
+            if (localStorage.getItem('token') == null) {
+                next({ name: 'login' })
+            } else {
+                next()
+            }
+        },
+        name: 'profile',
+        path: '/profile',
+        component: UserProfile
     }
 ]
 
