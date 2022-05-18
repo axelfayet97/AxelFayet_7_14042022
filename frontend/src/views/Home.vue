dd1c6e87d6be2d38fd1dfff7db3bf8bf15ecfc24
axelfayet97 <axel.gayet97@gmail.com>
2022-05-18 21:16:48 +0200

    Created component for new post

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,34 +1,9 @@
 <template>
   <h1>Bienvenue sur la home</h1>
-  <h2>Bonjour </h2>
   <section id="create-post">
     <div class="form_wrapper">
       <div class="form_wrapper_background">
-        <form class="connection_form"
-              @submit.prevent="sendPost">
-          <div id="textarea"
-               class="form_wrapper__field text_field">
-            <label for="text">Rédigez un post</label>
-            <input type="text"
-                   name="text"
-                   id="text"
-                   placeholder="À quoi pensez vous ?"
-                   v-model="content"
-                   required />
-          </div>
-          <div id="textarea">
-            <label for="file">Ajoutez une image</label>
-            <input type="file"
-                   name="image"
-                   id="file"
-                   v-on:change="imageUrl" />
-          </div>
-          <div class="form_wrapper__button submit__button">
-            <input type="submit"
-                   id="submit"
-                   value="Envoyer !" />
-          </div>
-        </form>
+        <PostForm />
       </div>
     </div>
   </section>
@@ -39,29 +14,20 @@
 </template>
 
 <script>
-import Post from '../components/Post.vue'
-import axios from 'axios'
+import Post from '@/components/Post.vue'
+import PostForm from '@/components/PostForm.vue'
 
 export default {
-  name: 'PostForm',
+  name: 'Home',
+  components: {
+    Post,
+    PostForm
+  },
   data() {
     return {
-      content: "",
-      imageUrl: "",
+      firstName: '',
+      lastName: ''
     }
-  },
-  methods: {
-    async sendPost() {
-      const response = await axios.post('posts', {
-        content: this.content,
-        imageUrl: this.imageUrl
-      })
-      console.log(response);
-      this.$router.push('/')
-    }
-  },
-  components: {
-    Post
   }
 }
 </script>
