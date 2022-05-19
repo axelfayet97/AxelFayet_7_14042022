cc6f78116c2cc3a3df8ffd97f1d9155bad792891
axelfayet97 <axel.gayet97@gmail.com>
2022-05-19 11:09:51 +0200

    Added scope style

--- frontend/src/components/PostForm.vue
+++ frontend/src/components/PostForm.vue
@@ -1,21 +1,21 @@
 <template>
     <form class="connection_form"
           @submit.prevent="sendPost">
-        <div id="textarea"
-             class="form_wrapper__field text_field">
-            <label for="text">Rédigez un post</label>
-            <input type="text"
-                   name="text"
-                   id="text"
-                   placeholder="À quoi pensez vous ?"
-                   v-model="content"
-                   required />
+        <div class="form_wrapper__field textarea_wrapper">
+            <label for="textarea">Rédigez un post</label>
+            <textarea name="textarea"
+                      id="textarea"
+                      cols="30"
+                      rows="10"
+                      placeholder="À quoi pensez-vous ?"
+                      v-model="content"
+                      required />
         </div>
-        <div id="textarea">
+        <div class="form_wrapper__field file-upload_wrapper">
             <label for="file">Ajoutez une image</label>
             <input type="file"
                    name="image"
-                   id="file"
+                   id="file-upload"
                    v-on:change="imageUrl" />
         </div>
         <div class="form_wrapper__button submit__button">
@@ -26,6 +26,30 @@
     </form>
 </template>
 
+<style scoped>
+#textarea {
+    min-width: 80%;
+    max-width: 80%;
+    min-height: 100px;
+    max-height: 20vh;
+    border: none;
+    border-bottom: 1px solid var(--noir);
+    font-family: 'Raleway', sans-serif;
+    font-size: 14px;
+    transition: all .2s linear;
+}
+
+#textarea:hover,
+#textarea:focus,
+#textarea:active {
+    border-color: var(--rouge);
+}
+
+#file-upload {
+    font-family: 'Raleway', sans-serif;
+    border: none;
+}
+</style>
 <script>
 import axios from 'axios'
 
