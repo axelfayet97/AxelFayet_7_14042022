6f581ce6062132b570c717c7b18883089ef96a1b
axelfayet97 <axel.gayet97@gmail.com>
2022-05-19 12:37:33 +0200

    Added admin

--- backend/app/middlewares/auth.middleware.js
+++ backend/app/middlewares/auth.middleware.js
@@ -5,32 +5,30 @@ const User = db.users;
 // MIDDLEWARE D'AUTHENTIFICATION
 module.exports = (req, res, next) => {
     // TO DO : IMPLEMENTATION IS ADMIN : req.body.isAdmin ? next() : else try catch
-    User.findOne().then(user => {
-        // if (user.dataValues.isAdmin) {
-        //     console.log('Admin auth');
-        //     // req.auth = {userId} ??
-        //     next();
-        // } else {
-            try {
-                // On récupère la valeur du token située après le Bearer
-                const token = req.headers.authorization.split(' ')[1];
-                // On décode le token avec la méthode verify en comparant le token à l'aide de la clé secrète
-                const decodedToken = jwt.verify(token, secretToken);
-                // On récupère et on stocke l'ID de l'utilisateur
-                const userId = decodedToken.userId;
-                // Protection des routes DELETE et UPDATE via intervention tierce
-                req.auth = { userId };
-                // Si l'ID du token est différent de celui dans le corps de la requête
-                if (req.body.userId && req.body.userId !== userId) {
-                    throw 'User ID non valable !';
-                } else {
-                    // Sinon next middleware
-                    next();
-                }
-            } catch (error) {
-                // Si une erreur survient...
-                res.status(401).json({ error: error || 'Requête non authentifiée !' });
-            };
-        // }
-    })
+    console.log(req.headers.authorization.split(' ')[1]);
+    try {
+        // On récupère la valeur du token située après le Bearer
+        const token = req.headers.authorization.split(' ')[1];
+        // On décode le token avec la méthode verify en comparant le token à l'aide de la clé secrète
+        const decodedToken = jwt.verify(token, secretToken);
+        // On récupère et on stocke l'ID de l'utilisateur
+        const userId = decodedToken.userId;
+        // Protection des routes DELETE et UPDATE via intervention tierce
+        req.auth = { userId };
+        // Si l'ID du token est différent de celui dans le corps de la requête
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'User ID non valable !';
+        } else {
+            // Sinon next middleware
+            User.findOne({ where: { id: userId } })
+                .then(user => {
+                    req.auth = { isAdmin: user.isAdmin }
+                })
+                .catch(error => { throw error })
+            next();
+        }
+    } catch (error) {
+        // Si une erreur survient...
+        res.status(401).json({ error: error || 'Requête non authentifiée !' });
+    };
 }
