2bef3132aa2683e2b62ea5ea57c41da4db29c357
axelfayet97 <axel.fayet97@gmail.com>
2022-05-20 10:00:41 +0200

    Still working on header issues

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:3000/api/'
+console.log('Token = ', axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token'))
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token')
-
 // Add dynamic headers
