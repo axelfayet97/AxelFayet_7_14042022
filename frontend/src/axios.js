c3ad4ca146085c8b4590c8685fba9f039452c810
axelfayet97 <axel.fayet97@gmail.com>
2022-05-22 22:50:05 +0200

    Debugging token issue

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -3,4 +3,4 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:3000/api/'
 console.log('Token = ', axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token'))
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token')
-// Add dynamic headers
+console.log('Axios : token set');
