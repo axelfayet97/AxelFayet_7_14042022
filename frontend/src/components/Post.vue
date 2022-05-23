8b14912c68393c7ed8d0cffd58381082019dc6ca
axelfayet97 <axel.gayet97@gmail.com>
2022-05-23 21:31:16 +0200

    Updated created at method, added comments button, added CSS, workling on comments display

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -11,7 +11,8 @@
                 </div>
                 <div class="author-infos">
                     <p id="post-author"> {{ post.user.firstName }} {{ post.user.lastName }}</p>
-                    <p id="created-at">{{ post.createdAt }}</p>
+                    <time id="created-at"
+                          :dateTime="post.createdAt">{{ post.createdAt }}</time>
                 </div>
             </div>
             <div class="container__header__options">
@@ -32,19 +33,26 @@
                   v-show="showOptions">
                 <textarea id="update-message">{{ post.updatedMessage }}</textarea>
                 <input type="submit"
-
                        value="Modifier">
             </form>
             <!-- <p v-if="post.imageUrl != null">{{ post.imageUrl }}</p> -->
         </div>
         <div class="container__controls">
-            <div id="like-post">
-                <a href="#"
-                   @click.prevent="likePost(post.id)">
-                    <img src="/noun-like-576529.svg"
-                         id="like-button" />
-                    {{ post.likes.length }}
-                </a>
+            <div class="controls-buttons">
+                <div id="comment-button">
+                    <img src="../../public/noun-comment-1935575.svg"
+                         alt="Bouton commentaire">
+                    {{ post.comments.length }}
+                </div>
+                <div id="like-post">
+                    <a href="#"
+                       @click.prevent="likePost(post.id)">
+                        <img src="/noun-like-576529.svg"
+                             id="like-button"
+                             alt="Bouton like" />
+                        {{ post.likes.length }}
+                    </a>
+                </div>
             </div>
             <Comment />
             <div id="comment-post">
@@ -60,7 +68,7 @@
                     <input type="submit"
                            value="Commenter"
                            id="comment-submit" />
-                    <div id="display-message">{{ this.displayMessage }}</div>
+                    <div id="error-message">{{ this.errorMessage }}</div>
                 </form>
             </div>
         </div>
@@ -68,12 +76,134 @@
 </template>
 
 <style scoped>
+/* POST CONTAINERA */
+.post-container {
+    max-width: 800px;
+    margin: 50px auto;
+    padding: 40px;
+    border-radius: 30px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.16);
+    background: #fff;
+}
+
+@media all and (max-width: 768px) {
+    .post-container {
+        max-width: 90%;
+    }
+}
+
+/* HEADER */
+
+.post-container__header {
+    display: flex;
+    justify-content: space-between;
+}
+
+.container__header__options a,
+.container__header__options a:visited {
+    cursor: pointer;
+    position: relative;
+    font-size: 30px;
+    font-weight: bold;
+    color: var(--gris);
+}
+
+.container__header__options a:hover,
+.container__header__options a:focus,
+.container__header__options a:active {
+    color: var(--noir)
+}
+
+.container__header__options a #controls {
+    position: absolute;
+    right: 50px;
+    top: 50%;
+    transform: translateY(-50%);
+    list-style-type: none;
+    color: var(--noir);
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    transition: right .2s linear;
+}
+
+.container__header__options a #controls li {
+    padding: 20px;
+}
+
+@media all and (max-width: 768px) {
+    .container__header__options a #controls {
+        display: block;
+        right: 0;
+        top: 70px;
+        text-align: right;
+    }
+
+    .container__header__options a #controls li {
+        padding: 10px;
+    }
+}
+
+
+#post-author {
+    font-weight: 800;
+}
+.post-container #author-img img,
+.post-container #comment-post img {
+    max-width: 50px;
+    margin-right: 20px;
+}
+
+.post-container__header__user-infos {
+    display: flex;
+}
+
+/* BODY */
+.post-container__body {
+    padding: 20px;
+    line-break: anywhere;
+}
+
+/* CONTROLS */
+
+.container__controls .controls-buttons {
+    display: flex;
+    justify-content: flex-end;
+    margin: 10px auto;
+}
+
+.container__controls .controls-buttons div {
+    margin: 0 10px;
+}
+
+.container__controls .controls-buttons #comment-button {
+    cursor: pointer;
+    transition: all .2s linear;
+}
+
+.container__controls .controls-buttons #comment-button:hover,
+.container__controls .controls-buttons #comment-button:focus,
+.container__controls .controls-buttons #comment-button:active {
+    color: var(--rouge)
+}
+
+.container__controls .controls-buttons img {
+    width: 30px;
+    vertical-align: middle;
+}
+
 #comments-form {
     display: flex;
     width: 100%;
 }
 
-.post-container #comment-post input[type="text"] {
+@media all and (max-width: 768px) {
+    #comments-form img {
+        width: 40px;
+        height: 40px;
+    }
+}
+#comment-post input[type="text"] {
     border: none;
     border-bottom: 1px solid var(--noir);
     background: transparent;
@@ -84,22 +214,22 @@
     text-overflow: ellipsis;
 }
 
-.post-container #comment-post #comment-text {
+#comment-post #comment-text {
     text-align: left;
     width: 100%;
 }
 
-.post-container #comment-post #comment-text::placeholder {
+#comment-post #comment-text::placeholder {
     font-family: "Raleway", sans-serif;
 }
 
-.post-container #comment-post #comment-text:hover,
-.post-container #comment-post #comment-text:focus,
-.post-container #comment-post #comment-text:active {
+#comment-post #comment-text:hover,
+#comment-post #comment-text:focus,
+#comment-post #comment-text:active {
     border-color: var(--rouge);
 }
 
-.post-container #comment-post #comment-submit {
+#comment-post #comment-submit {
     background: #FF0000;
     border-radius: 40px;
     border: 2px solid transparent;
@@ -111,15 +241,15 @@
     transition: all .2s linear;
 }
 
-.post-container #comment-post #comment-submit {
+#comment-post #comment-submit {
     margin: 0 0 0 20px;
     padding: 10px 20px;
     font-size: 14px;
 }
 
-.post-container #comment-post #comment-submit:hover,
-.post-container #comment-post #comment-submit:focus,
-.post-container #comment-post #comment-submit:active {
+#comment-post #comment-submit:hover,
+#comment-post #comment-submit:focus,
+#comment-post #comment-submit:active {
     background-color: #fff;
     color: var(--rouge);
     border: 2px solid var(--rouge);
@@ -139,7 +269,7 @@ export default {
             showOptions: false,
             isLiked: 0,
             commentContent: '',
-            displayMessage: '',
+            errorMessage: '',
             updatedMessage: ''
         }
     },
@@ -151,12 +281,6 @@ export default {
         const response = await axios.get('posts')
         const posts = await response.data
         this.posts = posts
-        posts.forEach(post => {
-            const date = post.createdAt
-            const newDate = date.slice['-']
-
-            console.log(newDate);
-        });
     },
     methods: {
         toggleControls() {
@@ -169,30 +293,22 @@ export default {
             })
             const likes = await response.data.data
             this.isLiked = likes
-            this.$router.go(`/${postId}`)
+            // this.$router.go(`/#${postId}`)
         },
-        commentPost(postId) {
-            axios.post('comments', {
-                postId,
+        async commentPost(postId) {
+            const response = await axios.post('comments', {
+                postId: postId,
                 content: this.commentContent
-            }).then(response => {
-                console.log(response);
-                document.getElementById('display-message').classList.add('successful-connection')
-                this.$router.go(`/${postId}`)
             })
-                .catch(error => {
-                    return this.displayMessage = 'Une erreur s\'est produite ' + error
-                })
-            // console.log(response);
+            console.log('Comment response = ', response, postId);
         },
         async updatePost(postId) {
             const data = {
                 content: this.updatedMessage,
                 // imageUrl: this.imageUrl,
             }
-            console.log(data);
             await axios.put(`posts/${postId}`, data)
-            // this.$router.go(`/#${postId}`)
+            this.$router.go(`/#${postId}`)
         },
         async deletePost(postId) {
             if (confirm('Voulez-vous vraiment supprimmer ce post ?')) {
