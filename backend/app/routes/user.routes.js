89d1b40b6829ae6856c58d4b7be534f5dbe8f1d8
axelfayet97 <axel.fayet97@gmail.com>
2022-05-23 09:45:13 +0200

    Installed user rate limit package

--- backend/app/routes/user.routes.js
+++ backend/app/routes/user.routes.js
@@ -3,9 +3,10 @@ module.exports = app => {
     const userCtrl = require('../controllers/user.controller');
     const auth = require('../middlewares/auth.middleware');
     const multer = require('../middlewares/multer.middleware');
+    const rateLimit = require('../middlewares/rateLimit.middleware')
     var router = require('express').Router();
     router.post('/signup', userCtrl.signup);
-    router.post('/login', userCtrl.login);
+    router.post('/login', rateLimit, userCtrl.login);
     router.put('/:id', auth, multer, userCtrl.modifyAccount);
     router.get('/', auth, userCtrl.getAccounts);
     router.get('/:id', auth, userCtrl.getOneAccount);
