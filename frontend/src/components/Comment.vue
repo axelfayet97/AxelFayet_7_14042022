39c96a2021181930930257aa2f9a213d8226aec3
axelfayet97 <axel.gayet97@gmail.com>
2022-05-25 08:36:20 +0200

    Displayed comments, working on a method that can display options for this comment + css

--- frontend/src/components/Comment.vue
+++ frontend/src/components/Comment.vue
@@ -11,21 +11,31 @@
                 </div>
                 <div class="comment-container__body">
                     <div class="author-infos">
-                        <p id="comment-author"> {{ comment.firstName }} {{ comment.lastName }}</p>
+                        <p id="comment-author"> {{ comment.user.firstName }} {{ comment.user.lastName }} <span class="span-time-comment"><time id="created-at"
+                          :dateTime="comment.createdAt">{{ comment.createdAt }}</time></span> </p>
                     </div>
                     <p>{{ comment.content }}</p>
-                    <!-- <p v-if="comment.imageUrl != null">{{ comment.imageUrl }}</p> -->
-                </div>
-                <div class="container__header__options">
-                    <a href="#"
-                       @click.prevent="toggleControls"
-                       id="toggle-controls"><span>...</span>
-                        <ul id="controls"
-                            v-show="showOptions">
-                            <li>Modifier</li>
-                            <li @click="deleteComment(comment.id)">Supprimer</li>
-                        </ul>
-                    </a>
+                    <div class="container__header__options"
+                         @vnodeMounted="belongsToUser(comment.userId)"
+                         v-if="displayOptions">
+                        <a href="#"
+                           @click.prevent="toggleControls"
+                           id="toggle-controls"><i class="bi bi-gear"></i>
+                            <ul id="controls"
+                                v-show="showOptions">
+                                <li><i @click.stop.prevent="editCommentControls"
+                                       class="bi bi-pencil"></i></li>
+                                <li @click="deleteComment(comment.id)"><i class="bi bi-trash3"></i></li>
+                            </ul>
+                        </a>
+                    </div>
+                    <form id="update-comment"
+                          v-show="editComment"
+                          @submit.prevent="updateComment(comment.id)">
+                        <textarea v-model="updatedMessage">{{ comment.content }}</textarea>
+                        <input type="submit"
+                               value="Modifier">
+                    </form>
                 </div>
             </div>
         </div>
@@ -46,14 +56,21 @@
     width: 100%;
 }
 
+.container__header__options {
+    position: absolute;
+    right: 20px;
+    top: 15px;
+}
+
 .comment-container__header__user-infos {
     display: flex;
-    align-items: center;
+    align-items: start;
     width: 100%
 }
 
 .comment-container #author-img img {
     width: 40px;
+    margin-top: 10px;
     margin-right: 10px;
 }
 
@@ -61,6 +78,17 @@
     font-weight: 600;
 }
 
+.span-time-comment {
+    font-weight: 300;
+    font-size: 12px;
+    color: var(--gris)
+}
+
+@media all and (max-width: 768px) {
+    .span-time-comment {
+        display: block;
+    }
+}
 
 .comment-container .comment-container__body {
     margin: 0 20px;
@@ -73,6 +101,33 @@
     border-radius: 30px;
     width: 100%
 }
+
+.comment-container__body .container__header__options a {
+    font-size: 18px;
+}
+
+.comment-container__body .container__header__options a #controls {
+    font-size: 15px;
+    right: 30px;
+}
+
+.comment-container__body .container__header__options a #controls li {
+    padding: 5px;
+}
+#update-post textarea {
+    min-width: 100%;
+    max-width: 100%;
+    min-height: 100px;
+    max-height: 250px;
+    font-family: 'Raleway', sans-serif;
+    padding: 10px;
+}
+
+#update-post input[type="submit"] {
+    display: block;
+    margin-left: auto;
+    margin-top: 10px !important;
+}
 </style>
 
 <script>
@@ -84,8 +139,11 @@ export default {
         return {
             comments: [],
             showOptions: false,
+            displayOptions: true,
+            editComment: false,
             commentContent: '',
-            displayMessage: ''
+            displayMessage: '',
+            updatedMessage: ''
         }
     },
     async mounted() {
@@ -93,33 +151,29 @@ export default {
         const response = await axios.get('comments')
         const comments = await response.data
         this.comments = comments
-        console.log(comments);
     },
     methods: {
         toggleControls() {
             this.showOptions = !this.showOptions
         },
-        // async modifyComment() {
-        //     const data = {
-        //         content: this.content,
-        //         imageUrl: this.imageUrl,
-        //     }
-        //     await axios.put(`comments/${this.id}`, data)
-        //     this.$router.go()
-        // },
+        editCommentControls() {
+            this.editComment = !this.editComment
+        },
+        belongsToUser(userId) {
+            userId == localStorage.getItem('userId') ? this.displayOptions = true : this.displayOptions = false
+        },
+        async updateComment(commentId) {
+            await axios.put(`comments/${commentId}`, { content: this.updatedMessage, })
+            this.$router.go(`/#${commentId}`)
+        },
         async deleteComment(commentId) {
             if (confirm('Voulez-vous vraiment supprimmer ce commentaire ?')) {
                 await axios.delete(`comments/${commentId}`)
-                //  {
-                //     headers: {
-                //         Authorization: localStorage.getItem('token')
-                //     }
-                // })
                 this.$router.go()
             } else {
                 return
             }
-        }
+        },
     },
 }
 </script>
