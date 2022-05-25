0800b5cccdaac6d9e64ac4b5e4aa58affb6432d0
axelfayet97 <axel.gayet97@gmail.com>
2022-05-25 08:40:49 +0200

    Secured get route

--- backend/app/routes/like.routes.js
+++ backend/app/routes/like.routes.js
@@ -4,6 +4,6 @@ module.exports = app => {
     const auth = require('../middlewares/auth.middleware');
     var router = require('express').Router();
     router.post('/', auth, likesCtrl.likeState);
-    router.get('/:id', likesCtrl.getLikes)
+    router.get('/:id', auth, likesCtrl.getLikes)
     app.use('/api/likes', router);
 };
